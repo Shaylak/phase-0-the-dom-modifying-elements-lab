2449c7af5b7d93c8492f2771857d87a9f172b512
Shaylak <shayyylaaaking@gmail.com>
2023-06-18 13:21:42 -0700

    save

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = `Shayla is the champion`;
